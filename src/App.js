2c1965c3eb4f4a75e0c283c6f9aa2583b7e9aaa0
Ar4m <a-j.tosbath@orange.fr>
2020-10-11 07:20:56 +0200

    Front

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
+import earth from './earth-round-02.png';
+import space from './space_background.jpg';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage:`url(${space})` }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={earth} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Site Front Simple
         </p>
+        <br/>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/IIM-Creative-Technology/devops-Ar4m"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          https://github.com/IIM-Creative-Technology/devops-Ar4m
         </a>
       </header>
     </div>
